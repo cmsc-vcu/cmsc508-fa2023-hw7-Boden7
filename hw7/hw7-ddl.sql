1514a74b7d132d9ee2b8096b7d4916d83f718737
Boden <bakahn973@comcast.net>
2023-11-12 22:32:52 -0500

    work part 1

--- hw7/hw7-ddl.sql
+++ hw7/hw7-ddl.sql
@@ -17,7 +17,11 @@
 # Drops all tables.  This section should be amended as new tables are added.
 
 SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS peopleroles;
+DROP TABLE IF EXISTS peopleskills;
 DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS skills;
+DROP TABLE IF EXISTS roles;
 # ... 
 SET FOREIGN_KEY_CHECKS=1;
 
@@ -28,11 +32,28 @@ SET FOREIGN_KEY_CHECKS=1;
 # time committment offers some sense of how much time was required (or will be required) to gain the skill.
 # You can assign the skill descriptions.  Please be creative!
 
+drop table if exists skills;
+create table skills(
+    id int NOT NULL,
+    name varchar(255) NOT NULL,
+    description varchar(255) NOT NULL DEFAULT '(default description)',
+    tag varchar(255) NOT NULL,
+    primary key(id)
+);
 
 # Section 3
 # Populate skills
 # Populates the skills table with eight skills, their tag fields must exactly contain “Skill 1”, “Skill 2”, etc.
 # You can assign skill names.  Please be creative!
+
+#Change these skills
+insert into skills (id, name, tag) values
+    (1, 'rock climbing', 'Skill 1'),
+    (2, 'mind reading', 'Skill 2'),
+    (3, 'cooking', 'Skill 3'),
+    (4, 'coding with python', 'Skill 4'),
+    (5, 'break dancing', 'Skill 5'),
+    (6, 'drawing', 'Skill 6');
 
 
 # Section 4
@@ -40,6 +61,7 @@ SET FOREIGN_KEY_CHECKS=1;
 # ID cannot be null, Last name cannot be null, date joined cannot be NULL.
 # All other fields can default to NULL.
 
+drop table if exists people;
 CREATE TABLE people (
     people_id int,
     people_last_name varchar(256) NOT NULL,
@@ -51,13 +73,29 @@ CREATE TABLE people (
 # Their last names must exactly be “Person 1”, “Person 2”, etc.
 # Other fields are for you to assign.
 
-insert into people (people_id,people_last_name) values (1,'Person 1');
-
+insert into people (people_id,people_last_name) values 
+    (1,'Person 1'),
+    (2,'Person 2'),
+    (3,'Person 3'),
+    (4,'Person 4'),
+    (5,'Person 5'),
+    (6,'Person 6');
 
 # Section 6
 # Create peopleskills( id, skills_id, people_id, date_acquired )
 # None of the fields can ba NULL. ID can be auto_increment.
 
+drop table if exists peopleskills;
+create table peopleskills (
+    id int auto_increment,
+    skills_id int,
+    people_id int,
+    date_acquired date default (current_date),
+    primary key (id),
+    foreign key (skills_id) references skills (id) on delete cascade,
+    foreign key (people_id) references people (people_id),
+    unique (skills_id, people_id)
+);
 
 # Section 7
 # Populate peopleskills such that:
@@ -73,6 +111,67 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 10 has skills 1,4,5;
 # Note that no one has yet acquired skills 7 and 8.
  
+ insert into peopleskills (people_id, skills_id) values
+    (1, 1),
+    (1, 3),
+    (1, 6),
+    (2, 3),
+    (2, 4),
+    (2, 5),
+    (3, 1),
+    (3, 5),
+    (5, 3),
+    (5, 6),
+    (6, 2),
+    (6, 3),
+    (6, 4),
+    (7, 3),
+    (7, 5),
+    (7, 6),
+    (8, 1),
+    (8, 3),
+    (8, 5),
+    (8, 6),
+    (9, 2),
+    (9, 5),
+    (9, 6),
+    (10, 1),
+    (10, 4),
+    (10, 5);
+
+select * from peopleskills;
+select count(*) from peopleskills;
+
+SELECT
+    people_last_name,
+    name,
+    tag
+from
+    peopleskills a
+    inner join people b on (a.people_id = b.people_id)
+    inner join skills c on (a.skills_id = c.id)
+    order BY
+        name,
+        people_last_name;
+
+SELECT
+    people_last_name
+from
+    people a
+        left join peopleskills b on (a.people_id = b.people_id)
+Where
+    b.people_id is NULL;
+
+SELECT
+    name,
+    count(*)
+from
+    peopleskills a
+    inner join people b on (a.people_id = b.people_id)
+    inner join skills c on (a.skills_id = c.id)
+group BY
+    name;
+
 
 # Section 8
 # Create roles( id, name, sort_priority )
